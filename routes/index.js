699a13b14159ab01afef0e7cd75c41691c0e2873
Pixel <petngalan@gmail.com>
2023-07-30 17:10:58 +0200

    Create the api routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.send("NOT IMPLEMENTED YET: GET Homepage");
+});
+
+router.get("/posts", function (req, res, next) {
+  res.send("NOT IMPLEMENTED YET: GET Posts");
+});
+
+router.post("/posts", function (req, res, next) {
+  res.send("NOT IMPLEMENTED YET: POST a Post");
+});
+
+router.get("/posts/:id", function (req, res, next) {});
+res.send("NOT IMPLEMENTED YET: GET a Post");
+
+router.put("/posts/:id", function (req, res, next) {
+  res.send("NOT IMPLEMENTED YET: UPDATE a Post");
+});
+
+router.delete("/posts/:id", function (req, res, next) {
+  res.send("NOT IMPLEMENTED YET: DELETE a Post");
+});
+
+router.get("/posts/:id/comments", function (req, res, next) {
+  res.send("NOT IMPLEMENTED YET: GET comments");
+});
+
+router.post("/posts/:id/comments", function (req, res, next) {
+  res.send("NOT IMPLEMENTED YET: POST a comment");
 });
 
 module.exports = router;
